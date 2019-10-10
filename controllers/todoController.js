e113bee31b8c76ebce6d4978d7bba503c84f4e96
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-10 00:01:13 -0600

    System Updated

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,20 @@
+let dataStorage = [
+  {item: 'Get Milk'},
+  {item: 'Walk Dog'},
+  {item: 'Kick some coding ass'}
+]
+
 module.exports = (app) => {
   let bodyParser = require('body-parser')
   let urlencoded = bodyParser.urlencoded({extended: false});
 
   app.get('/todo', (req, res) => {
-    res.render('todo')
+    res.render('todo', {data: dataStorage})
   });
 
   app.post('/todo', urlencoded, (req, res) => {
 
+    res.render('todo', {item: req.body});
   });
 
   app.delete('/todo', (req, res) => {
